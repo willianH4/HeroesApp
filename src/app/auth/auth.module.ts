730cd02258659b3da1df4750ad75fbdc1d0fc2b0
Willian <williamhernandez450@gmail.com>
2022-03-11 06:59:04 -0600

    Fixed bug lazy load

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './pages/login/login.component';
+import { RegistroComponent } from './pages/registro/registro.component';
+import { AuthRoutingModule } from './auth-routing.module';
 
 
 
 @NgModule({
   declarations: [
-    LoginComponent
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AuthRoutingModule
+  ],
+  exports: [
+
   ]
 })
 export class AuthModule { }
